b2b8674b301ec9eb2824045aa294dface1b9feaf
Olaska-js <jitendra.developer77@gmail.com>
2023-05-16 04:47:49 -0700

    python tocken file commit

---
+++ python-tocken.py
@@ -0,0 +1,51 @@
+# Python Tokens
+
+# Keywords
+# Avoid using keywords as variable names
+#True = a  
+
+# Identifiers
+# Variable storing the value "martha"
+student = "martha"  
+# Printing the value of the variable "student"
+print(student)  
+
+# Capitalization matters in Python, different variable from 
+Student = "julie"  
+# Printing the value of the variable "Student"
+print(Student)  
+
+
+# Literals
+# Variable storing the string "Hello World!!"
+a = "Hello World!!"  
+# Variable storing the float value 3.12
+a = 3.12  
+# Printing the value of the variable "a"
+print(a)  
+
+
+# Variable storing a string enclosed in single quotes
+str1 = 'this is my first string'  
+# Variable storing a string enclosed in double quotes
+str2 = "this is my second string"  
+# Variable storing a multiline string using triple quotes
+str3 = """
+This string
+has
+a lot of
+lines
+in it
+"""  
+
+# Printing the value of the variable "str1"
+print(str1)  
+# Printing the value of the variable "str2"
+print(str2)  
+# Printing the value of the variable "str3"
+print(str3)  
+
+# Variable storing the string "This is Sparta"
+my_string = "This is Sparta"  
+
+# Overall, the code assigns values to variables and prints their values.
