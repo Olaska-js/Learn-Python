51a12d2ed3ffad69ffa74f003a90c78c4a0114e8
Olaska-js <jitendra.developer77@gmail.com>
2023-05-16 04:39:51 -0700

    operators file commit

---
+++ operators.py
@@ -0,0 +1,45 @@
+# Arithmetic Operators (+ - * /)
+a = 10
+b = 20
+# a + b performs addition between variables a and b.
+print(a + b)
+# a - b performs subtraction between variables a and b.
+print(a - b)
+# a * b performs multiplication between variables a and b.
+print(a * b)
+# a / b performs division between variables a and b.
+print(a / b)
+
+
+# Relational Operators (< > == !=)
+a = 50
+b = 100
+# a < b checks if a is less than b.
+print(a < b)
+# a > b checks if a is greater than b.
+print(a > b)
+# a == b checks if a is equal to b.
+print(a == b)
+# a != b checks if a is not equal to b.
+print(a != b)
+
+
+# Logical Operators (& |)
+a = True
+b = False
+# Logical AND: Both operands need to be True for the result to be True
+print(a & b)  # False
+# Logical OR: At least one operand needs to be True for the result to be True
+print(a | b)  # True
+# Logical AND: Both operands need to be True for the result to be True
+print(b & a)  # False
+# Logical AND: Both operands are True, so the result is True
+print(a & a)  # True
+# Logical AND: Both operands are False, so the result is False
+print(b & b)  # False
+# Logical OR: At least one operand is True, so the result is True
+print(b | a)  # True
+# Logical OR: Both operands are False, so the result is False
+print(b | b)  # False
+# Logical OR: Both operands are True, so the result is True
+print(a | a)  # True
