18e5d1183de9e3e829a967941310fba1a98bd6c7
Olaska-js <jitendra.developer77@gmail.com>
2023-05-16 04:22:36 -0700

    data type file commit

---
+++ data-types.py
@@ -0,0 +1,26 @@
+# Data Types:
+
+# a1 = 10 assigns the integer value 10 to the variable a1.
+# Integers
+a1 = 10
+print(type(a1))
+
+# a1 = 152.564 assigns the float value 152.564 to the variable a1.
+# Float
+a1 = 152.564
+print(type(a1))
+
+# a1 = True assigns the Boolean value True to the variable a1.
+# Boolean
+a1 = True
+print(type(a1))
+
+# a1 = "Hello World!!" assigns the string value "Hello World!!" to the variable 
+# String
+a1 = "Hello World!!"
+print(type(a1))
+
+# a1 = 3 + 1j assigns the complex number 3 + 1j to the variable a1.
+# Complex
+a1 = 3 + 1j
+print(type(a1))
