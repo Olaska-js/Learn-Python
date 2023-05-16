5700f1fcd1839886b3982e0e1f02c8596ff2b99b
Olaska-js <jitendra.developer77@gmail.com>
2023-05-16 04:15:38 -0700

    first commit

---
+++ variables-assign.py
@@ -0,0 +1,21 @@
+# ROOLS
+# Variable names must start with a letter or underscore (_).
+# Variable names can contain letters, numbers, and underscores.
+# Variable names are case-sensitive, meaning myVar and myvar are different variables.
+# Avoid using reserved keywords as variable names, such as if, else, while, etc.
+# Choose descriptive and meaningful names for variables to improve code readability.
+name = "John"
+age = 25
+average_score = 8.5
+is_student = True
+
+# Variables and Assignment:
+
+# student = "sam" assigns the string value "sam" to the variable student.
+# student = "bob" reassigns the variable student with the string value "bob".
+student = "sam"
+print(student)
+
+student = "bob"
+print(student)
+
